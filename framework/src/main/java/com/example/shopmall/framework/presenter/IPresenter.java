b136a0630cdb22da595c9e2294ae3d9af8619849
胡显晴 <320805621@qq.com>
2020-04-25 10:22:23 +0800

    切换

--- framework/src/main/java/com/example/shopmall/framework/presenter/IPresenter.java
+++ framework/src/main/java/com/example/shopmall/framework/presenter/IPresenter.java
@@ -2,9 +2,8 @@ package com.example.shopmall.framework.presenter;
 
 public interface IPresenter {
     // requestCode 区分不同返回 数据类型
-    void getHttpData(int requestCode);
-    void postHttpData(int requestCode);
-    void postHttpDataWithJson(int requestCode);
-
-    void destroy();
+    void getHttpData(int requestCode); // get请求
+    void postHttpData(int requestCode); // post请求 表单
+    void postHttpDataWithJson(int requestCode); // post请求 json
+    void destroy(); // 销毁
 }
